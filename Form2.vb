ae68a4650b4db62e5a9bba1ee526502535080711
ChanKinWah0526 <99315061+ChanKinWah0526@users.noreply.github.com>
2022-02-09 16:01:47 +0800

    Add files via upload

---
+++ Form2.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form2
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
